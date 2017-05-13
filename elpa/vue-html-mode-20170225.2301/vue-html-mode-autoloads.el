f3f8df44185364b4f559ddbe556b609926556f87
WayneVN <cmcodevn@gmail.com>
2017-05-13 19:52:34 +0800

    init

---
+++ elpa/vue-html-mode-20170225.2301/vue-html-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; vue-html-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "vue-html-mode" "vue-html-mode.el" (22789 29967
+;;;;;;  805608 65000))
+;;; Generated autoloads from vue-html-mode.el
+
+(autoload 'vue-html-mode "vue-html-mode" "\
+Major mode for Vue.js templates
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("vue-html-mode-pkg.el") (22789 29967 393601
+;;;;;;  592000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; vue-html-mode-autoloads.el ends here
